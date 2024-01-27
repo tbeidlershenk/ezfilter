793d41d174d5a1667dadb06a2648fcf32d1e73dc
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2024-01-27 14:28:00 -0500

    Add google sign in

--- react-app/front-end/src/App.js
+++ react-app/front-end/src/App.js
@@ -1,33 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleOneTapLogin } from "@react-oauth/google";
+import {
+  GoogleAuth,
+  GoogleButton,
+  GoogleAuthConsumer,
+  IOAuthState,
+} from "react-google-oauth2";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
   const [first, setFirst] = useState(true);
-  const [second, setSecond] = useState(true);
-
+  const [second, setSecond] = useState(false);
+  const [query, setQuery] = useState("");
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
 
+    const handleChange=(data)=>{
+        console.log(data); {/* this just prints what box they checked into the console*/}
+    }
+    const options = {
+      //clientId: (process.env.CLIENT_ID),
+      clientId: "1088400508244-eotoi28bfta05ac306sjukpvufst9aq2.apps.googleusercontent.com",
+      redirectUri: "http://localhost:3000",
+      scopes: ["openid", "profile", "email"],
+      includeGrantedScopes: true,
+      accessType: "offline",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
 
         ... no changes in this part ...
 
         <p>The current time is {currentTime}.</p>
-      </header>
+      </header> */}
+      <label class="dynamic"> Search </label>
+      <input class="dynamic" type="text" onChange={e => setQuery(e.target.value)}/>
       <div>EzFilter
         {/* header - textbox w search functionality */}
         {/* filter options */}
-        <input type="checkbox" value={first} onChange={()=> handleChange("first")}/> First
-        <input type="checkbox" value={second} onChange={()=> handleChange("second")}/> Second
-
-        </div>
+        <GoogleButton
+              placeholder="demo/search.png" // Optional
+              options={options}
+              apiUrl="http://localhost:5000/google_login"
+              defaultStyle={true} // Optional
+          />
+      </div>
     </div>
 
     
