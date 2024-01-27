db5cbcd63575ee5a9d9e107925f3e8235a83f7e8
Ruchi Hegde <91853648+ruh32@users.noreply.github.com>
2024-01-27 18:11:14 -0500

    added search bar (again)

--- react-app/front-end-NEW/auth.js
+++ react-app/front-end-NEW/auth.js
@@ -124,3 +124,100 @@ async function listLabels() {
   );
   document.getElementById("content").innerText = output;
 }
+
+// stuff for UI
+
+const people = [
+  { name: 'adri'},
+  { name: 'becky'},
+  { name: 'chris'},
+  { name: 'dillon'},
+  { name: 'evan'},
+  { name: 'frank'},
+  { name: 'georgette'},
+  { name: 'hugh'},
+  { name: 'igor'},
+  { name: 'jacoby'},
+  { name: 'kristina'},
+  { name: 'lemony'},
+  { name: 'matilda'},
+  { name: 'nile'},
+  { name: 'ophelia'},
+  { name: 'patrick'},
+  { name: 'quincy'},
+  { name: 'roslyn'},
+  { name: 'solene'},
+  { name: 'timothy'},
+  { name: 'uff'},
+  { name: 'violet'},
+  { name: 'wyatt'},
+  { name: 'x'},
+  { name: 'yadri'},
+  { name: 'zack'},
+]
+const searchInput = document.querySelector('.input');
+searchInput.addEventListener("input", (e) => {
+  // inside, we will need to achieve a few things:
+  // 1. declare and assign the value of the event's target to a variable AKA whatever is typed in the search bar
+  let value = e.target.value
+
+  // 2. check: if input exists and if input is larger than 0
+  if (value && value.trim().length > 0){
+      // 3. redefine 'value' to exclude white space and change input to all lowercase
+       value = value.trim().toLowerCase()
+      // 4. return the results only if the value of the search is included in the person's name
+      // we need to write code (a function for filtering through our data to include the search input value)
+      setList(people.filter(person => {
+        return person.name.includes(value)
+    }));
+    } else {
+      // 5. return nothing
+      clearList()
+      // input is invalid -- show an error message or show no results
+
+  }
+
+});
+
+function noResults(){
+  // create an element for the error; a list item ("li")
+  const error = document.createElement('li')
+  // adding a class name of "error-message" to our error element
+  error.classList.add('error-message')
+
+  // creating text for our element
+  const text = document.createTextNode('No results found. Sorry!')
+  // appending the text to our element
+  error.appendChild(text)
+  // appending the error to our list element
+  list.appendChild(error)
+}
+
+const clearButton = document.getElementById('clear')
+
+clearButton.addEventListener("click", () => {
+  clearList()
+})
+
+function clearList(){
+  // looping through each child of the search results list and remove each child
+  while (list.firstChild){
+      list.removeChild(list.firstChild);
+  }
+}
+
+function setList(results){
+  clearList()
+    for (const person of results){
+        const resultItem = document.createElement('li')
+        resultItem.classList.add('result-item')
+        const text = document.createTextNode(person.name)
+        resultItem.appendChild(text)
+        list.appendChild(resultItem)
+    }
+
+    if (results.length === 0 ){
+        noResults()
+    }
+
+}
