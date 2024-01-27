c4c00fd13c8c658de82bf805f698ec987b3cdbdd
Ruchi Hegde <91853648+ruh32@users.noreply.github.com>
2024-01-27 08:46:27 -0500

    started fresh

--- react-app/front-end/src/index.js
+++ react-app/front-end/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a basic React app.</p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +21,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
